f068fe22dc0a4fc7a88db53dcee852067bbd6063
Johnny Bueti <29045904+Fehniix@users.noreply.github.com>
2021-12-10 14:58:11 +0100

    feat: improved test case

--- test/2.ReactiveDecorator.test.ts
+++ test/2.ReactiveDecorator.test.ts
@@ -2,38 +2,65 @@ import { expect } from 'chai';
 import { reactive } from '../lib/ReactiveDecorator';
 import Endpoint2 from './Endpoint2';
 
-type MyTestProp = {
-	num: number
-	word: string
+class PropWrapperEP1 {
+	@reactive('string')
+	public _string!: string;
+
+	@reactive('number')
+	public _number!: number;
+
+	@reactive('boolean')
+	public _boolean!: boolean;
+
+	@reactive('any')
+	public _any!: any;
 }
 
-class TestClass {
-	@reactive('testProp')
-	public reactiveEP1!: MyTestProp;
+class PropWrapperEP2 {
+	@reactive('string', Endpoint2.srInstance)
+	public _string!: string;
 
-	@reactive('testProp', Endpoint2.srInstance)
-	public reactiveEP2!: MyTestProp;
+	@reactive('number', Endpoint2.srInstance)
+	public _number!: number;
+
+	@reactive('boolean', Endpoint2.srInstance)
+	public _boolean!: boolean;
+
+	@reactive('any', Endpoint2.srInstance)
+	public _any!: any;
+}
+
+async function sleep(ms: number): Promise<void> {
+	return new Promise(r => setTimeout(r, ms));
 }
 
 describe('ReactiveDecorator', function() {
-	context('Given a reactive property', function() {
-		let testClass: TestClass;
+	let ep1: PropWrapperEP1;
+	let ep2: PropWrapperEP2;
 
-		this.beforeAll(() => {
-			testClass = new TestClass();
-			testClass.reactiveEP1 = {
-				num: 0,
-				word: "a"
-			};
+	this.beforeAll(() => {
+		ep1 = new PropWrapperEP1();
+		ep2 = new PropWrapperEP2();
+	});
+
+	describe('Given a string property on EP1', function() {
+		context('When first instantiated', function() {
+			it('should be correctly updated on EP2 as well', async function() {
+				ep1._string = 'a';
+
+				await sleep(600);
+
+				expect(ep2._string).to.equal('a');
+			});
 		});
 
-		context('When updated on EP1', function() {
-			it('should correctly update on EP2 as well', async function() {
-				testClass.reactiveEP1.num = 1;
+		context('When casually updated with the += operator', function() {
+			it('the change should be correctly implemented over on EP2 as well', async function() {
+				ep1._string += 'b';
 
-				await new Promise(r => setTimeout(r, 400));
+				await sleep(400);
 
-				expect(testClass.reactiveEP2?.num).to.equal(1);
+				expect(ep2._string).to.equal('ab');
 			});
 		});
 	});
